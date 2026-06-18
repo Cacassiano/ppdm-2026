5e0ba6ce1c8a3b092ce51f5e3296e32bcd919fde
Cacassiano <saurinho.64.ss@gmail.com>
2026-06-18 16:41:35 -0300

    Resolvendo essa atividade do dia 18-06

---
+++ atividade-18-06/App.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { ref, computed } from 'vue'
+import TaskList from './TaskList.vue';
+const taskList = ref(["Tarefas importantes"])
+const newList = ref("");
+const handleSubmit = (e) => {
+    e.preventDefault()
+    taskList.value.push(newList.value);
+    newList.value = "";
+  }
+</script>
+
+
+<template>
+  <form @submit="handleSubmit">
+    <input v-model="newList" placeholder="crie uma nova lista de Tarefas" required/>
+    <button>Create New list</button>
+  </form>  
+  <li v-for="list in taskList">
+    <TaskList :title="list" />
+  </li>
+</template>
